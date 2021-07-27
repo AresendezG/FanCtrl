2f2266d0d03e261e6a7fd8ffb7883ca959b1a09c
Alejandro Resendez <al.resendez.gro@gmail.com>
2021-07-27 14:15:30 -0500

    Export GPIO pins and add a function to check if there's a single instance execution or not

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 ﻿#include <wiringPi.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <iostream>
 #include <iomanip>
@@ -8,13 +9,14 @@
 #include <csignal>
 #include <cmath>
 
+
 /*
 * Run gpio readall first to make sure WiringPi is working
 * Export your GPIO pins so you can use them without sudo
 *  Define your pins based on the BCM_GPIO number
 */
 
-// Pin number definition
+// Pin number definition based on BCM_GPIO number
 #define	POWERLED	18
 #define FAN_CTRL    22
 #define ENABLE_PWM  23
@@ -39,11 +41,11 @@ void waitSecs(int Secs)
 }
 
 void ToggleStatus(int PinNumber) {
-    
-        bool currentStatus = digitalRead(PinNumber);
-        bool newStatus = !(currentStatus);
-        digitalWrite(PinNumber, newStatus);
-    
+
+    bool currentStatus = digitalRead(PinNumber);
+    bool newStatus = !(currentStatus);
+    digitalWrite(PinNumber, newStatus);
+
 }
 
 
@@ -65,6 +67,7 @@ double GetTempValue() {
 void GetInitConfigs(int* SettingsArray) {
 
     FILE* settingsFile;
+    char comment[200];
 
     settingsFile = fopen("/etc/fan_ctrl/fan_settings", "r");
     if (settingsFile == NULL)
@@ -77,22 +80,76 @@ void GetInitConfigs(int* SettingsArray) {
         std::cout << "Unable To Read default Settings /etc/fan_ctrl/fan_settings" << std::endl;
         return;
     }
-    fscanf(settingsFile, "%i\n%i\n%i\n%i", 
-        &SettingsArray[0], // Interval Wait
-        &SettingsArray[1], // Max Temp
-        &SettingsArray[2], // Mid Temp
-        &SettingsArray[3]); // Min Temp
+    fscanf(settingsFile, "%i %s\n%i %s\n%i %s\n%i %s\n",
+        &SettingsArray[0], // Interval Wait in Minutes (integer)
+        &comment,
+        &SettingsArray[1], // Max Temp in C (integer)
+        &comment,
+        &SettingsArray[2], // Mid Temp in C (integer)
+        &comment,
+        &SettingsArray[3],
+        &comment); // Min Temp
     // printf("The temperature is %6.3f C.\n", T);
     fclose(settingsFile);
 }
 
 
+int SingleInstanceCheck() {
+
+    FILE* ThePIDFile;
+    std::string comment;
+    int PID_ValueinFile;
+    pid_t CurrentProcess_PID;
+
+
+    ThePIDFile = fopen("/home/pi/Public/FanCtrl/fan_pid", "r");
+    if (ThePIDFile == NULL)
+    {
+        CurrentProcess_PID = getpid();
+        std::ofstream output("/home/pi/Public/FanCtrl/fan_pid");
+        output << CurrentProcess_PID;
+        return 150; // No file, continue execution
+
+    }
+    else {
+        fscanf(ThePIDFile, "%i", &PID_ValueinFile);
+        fclose(ThePIDFile);
+
+        if (kill(PID_ValueinFile, 0)) { // File exists, but PID is not active.
+           // std::cout << "This returned TRUE" << std::endl;
+            CurrentProcess_PID = getpid();
+            std::ofstream output("/home/pi/Public/FanCtrl/fan_pid");
+            output << CurrentProcess_PID;
+            return 150;
+        }
+        else {
+            std::cout << "Error. Active PID: " << PID_ValueinFile << std::endl;
+        }
+
+        return PID_ValueinFile;
+    }
+}
+
+
+
 int main(void)
 {
-    double CPU_Temp;
-	wiringPiSetupSys();
 
-	pinMode(POWERLED, OUTPUT);
+
+    int SingleInstanceFlag = SingleInstanceCheck();
+    if (SingleInstanceFlag == 150) {
+        std::cout << "Started Fan Control" << std::endl;
+    }
+    else {
+        std::cout << "Another Execution of this Program was Detected. " << SingleInstanceFlag << std::endl;
+        return -280;
+    }
+
+
+    double CPU_Temp;
+    wiringPiSetupSys();
+
+    pinMode(POWERLED, OUTPUT);
     pinMode(FAN_CTRL, OUTPUT);
     pinMode(ENABLE_PWM, OUTPUT);
 
@@ -109,7 +166,10 @@ int main(void)
     TempMiddle = Settings[2];
     TempLow = Settings[3];
 
-    std::cout << "Started Fan Control" << std::endl;
+
+    std::cout << "Enable Power LED" << std::endl;
+    digitalWrite(POWERLED, TRUE);
+
 
     digitalWrite(FAN_CTRL, TRUE);
     std::cout << "Enable Fan" << std::endl;
@@ -119,13 +179,13 @@ int main(void)
 
     // Temp Control Loop
 
-   while(true) {
+    while (true) {
 
         //ToggleStatus(POWERLED); Debug Only
         CPU_Temp = GetTempValue();
 
         if (CPU_Temp != -150) {
-            std::cout << "Current Temp Value: " << CPU_Temp <<" *c" <<std::endl;
+            std::cout << "Current Temp Value: " << CPU_Temp << " *c" << std::endl;
             if (CPU_Temp > TempMax) {
                 std::cout << "Set the Fans At MAX Power" << std::endl;
                 digitalWrite(ENABLE_PWM, FALSE);
@@ -147,7 +207,7 @@ int main(void)
         }
 
     }
-    
+
     std::cout << "Finished Loop";
 
 
